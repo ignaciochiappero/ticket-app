b09c17ceae17662ec6bca70dc6d50d103bedc580
nachochiappero <ignaciogabrielchiappero@gmail.com>
2026-09-16 23:24:49 -0300

    chore(api): connect to mongodb with mongoose

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI ?? 'mongodb://localhost:27017/tickets',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
